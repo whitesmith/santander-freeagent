4e18d83651a2ab1f3c61c6a9bbbdbbc65aaa42c7
Rafael Jegundo <rafael.jegundo@gmail.com>
2014-08-31 19:46:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SantanderUK.csv to FreeAgent.csv
+
+Converts bank statements from Santander UK in csv to a FreeAgent compatible
+format
+
+Inspired by https://github.com/dv/freeagent
---
+++ csv_converter_script.rb
@@ -0,0 +1,42 @@
+# Santander provides CSV statements with following columns
+
+# "Date","Narrative","Transaction Type","Debit","Credit","Current Balance"
+
+# Freeagent requires CSV statements as follows:
+
+# Date dd/mm/yyyy | amount (2 decimal places, negative or positive) | description
+
+require 'csv'
+require 'date'
+
+unless ARGV.length == 1
+  puts "Need an input CSV file"
+  exit
+end
+
+CSV.open(ARGV[0] + ".freeagent", "wb") do |output|
+  row_count = 0
+
+  CSV.foreach(ARGV[0], encoding: "ISO-8859-15:UTF-8",  col_sep: ",", headers: true) do |input|
+    row_count += 1
+    if row_count > 9
+      date        = Date.parse(input[0])
+      debit = input[3]
+      credit = input[4]
+      if debit == ""
+        amount = credit.gsub(",", ".").to_f
+      else
+        amount = debit.gsub(",", ".").to_f*(-1)
+      end
+
+      description = input[1]
+
+      row = []
+      row << date.strftime("%d/%m/%Y")
+      row << amount.round(2)
+      row << description
+      output << row
+    end
+  end
+
+end
